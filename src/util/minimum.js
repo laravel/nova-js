e49abb48d313b6ec83ece8f675e0a8bc0887b0c5
David Hemphill <davidlee.hemphill@gmail.com>
2018-06-27 14:51:48 -0500

    wip

--- src/util/minimum.js
+++ src/util/minimum.js
@@ -1,10 +1,10 @@
-export default function(originalPromise, delay = 1000) {
-  return Promise.all([
-    originalPromise,
-    new Promise(resolve => {
-      setTimeout(() => resolve(), delay)
-    }),
-  ]).then(result => result[0])
+export default function(originalPromise, delay = 250) {
+    return Promise.all([
+        originalPromise,
+        new Promise(resolve => {
+            setTimeout(() => resolve(), delay)
+        }),
+    ]).then(result => result[0])
 }
 
 // Usage
