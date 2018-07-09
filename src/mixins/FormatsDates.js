1c63660de3f0ec91fde0dce0df6d92a6df1e6b7e
David Hemphill <davidlee.hemphill@gmail.com>
2018-07-09 12:12:34 -0500

    wip

--- src/mixins/FormatsDates.js
+++ src/mixins/FormatsDates.js
@@ -5,63 +5,70 @@ export default {
      */
     toAppTimezone(value) {
       return value
-                  ? moment.tz(value, moment.tz.guess()).clone().tz(Nova.timezone).format('YYYY-MM-DD kk:mm:ss')
-                  : value
+        ? moment
+            .tz(value, moment.tz.guess())
+            .clone()
+            .tz(Nova.config.timezone)
+            .format('YYYY-MM-DD kk:mm:ss')
+        : value
     },
 
     /**
      * Convert the given application timezone date time string to the local timezone.
      */
     fromAppTimezone(value) {
-        if (! value) {
-            return value;
-        }
+      if (!value) {
+        return value
+      }
 
-        return moment.tz(value, Nova.timezone)
-                    .clone()
-                    .tz(moment.tz.guess())
-                    .format('YYYY-MM-DD kk:mm:ss')
+      return moment
+        .tz(value, Nova.config.timezone)
+        .clone()
+        .tz(moment.tz.guess())
+        .format('YYYY-MM-DD kk:mm:ss')
     },
 
     /**
      * Get the localized date time for the given field.
      */
     localizeDateTimeField(field) {
-        if (! field.value) {
-            return field.value;
-        }
+      if (!field.value) {
+        return field.value
+      }
 
-        const localized = moment.tz(field.value, Nova.timezone)
-                        .clone()
-                        .tz(moment.tz.guess())
+      const localized = moment
+        .tz(field.value, Nova.config.timezone)
+        .clone()
+        .tz(moment.tz.guess())
 
-        if (field.format) {
-            return localized.format(field.format)
-        }
+      if (field.format) {
+        return localized.format(field.format)
+      }
 
-        return this.twelveHourTime
-                    ? localized.format('YYYY-MM-DD h:mm:ss A')
-                    : localized.format('YYYY-MM-DD kk:mm:ss')
+      return this.twelveHourTime
+        ? localized.format('YYYY-MM-DD h:mm:ss A')
+        : localized.format('YYYY-MM-DD kk:mm:ss')
     },
 
     /**
      * Get the localized date for the given field.
      */
     localizeDateField(field) {
-        if (! field.value) {
-            return field.value;
-        }
+      if (!field.value) {
+        return field.value
+      }
 
-        const localized = moment.tz(field.value, Nova.timezone)
-                        .clone()
-                        .tz(moment.tz.guess())
+      const localized = moment
+        .tz(field.value, Nova.config.timezone)
+        .clone()
+        .tz(moment.tz.guess())
 
-        if (field.format) {
-            return localized.format(field.format)
-        }
+      if (field.format) {
+        return localized.format(field.format)
+      }
 
-        return localized.format('YYYY-MM-DD')
-    }
+      return localized.format('YYYY-MM-DD')
+    },
   },
 
   computed: {
@@ -69,8 +76,10 @@ export default {
      * Determine if the user is used to 12 hour time.
      */
     twelveHourTime() {
-        return _.endsWith(new Date().toLocaleString(), 'AM') ||
-               _.endsWith(new Date().toLocaleString(), 'PM')
-    }
-  }
+      return (
+        _.endsWith(new Date().toLocaleString(), 'AM') ||
+        _.endsWith(new Date().toLocaleString(), 'PM')
+      )
+    },
+  },
 }
