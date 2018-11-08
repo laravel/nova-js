24f9050322c916d261073da6e56f05345bba02fd
David Hemphill <davidlee.hemphill@gmail.com>
2018-11-08 09:56:25 -0600

    wip

--- src/mixins/Filterable.js
+++ src/mixins/Filterable.js
@@ -2,97 +2,57 @@ import each from 'lodash/each'
 import get from 'lodash/get'
 
 export default {
-  data: () => ({
-    filters: [],
-    currentFilters: [],
-  }),
-
   methods: {
-    /**
-     * Get the filters available for the current resource.
-     */
-    getFilters() {
-      // this.filters = []
-      // this.currentFilters = []
-
-      return Nova.request()
-        .get('/nova-api/' + this.resourceName + '/filters')
-        .then(response => {
-          // this.filters = response.data
-          // Nova.bus.filters[this.filterParameter]['filters'] = response.data
-          this.initializeFilterValuesFromQueryString()
-        })
-    },
-
-    /**
-     * Initialize the current filter values from the decoded query string.
-     */
-    initializeFilterValuesFromQueryString() {
-      console.log('initializing filter values from query string')
-      // this.clearAllFilters()
-      // if (this.encodedFilters) {
-      //   this.currentFilters = JSON.parse(atob(this.encodedFilters))
-      //   console.log(this.currentFilters)
-      //   this.syncFilterValues()
-      // }
-    },
-
     /**
      * Clear filters and reset the resource table
      */
-    clearSelectedFilters() {
-      //     console.log('clearing selected filters')
-      //     // Nova.$emit('clear-selected-filters')
-      //     this.filterChanged()
-      //     this.clearAllFilters()
-      //     // this.$nextTick(() => this.filterChanged())
-      //     // this.clearAllFilters()
-      //     this.syncFilterValues()
-    },
+    async clearSelectedFilters() {
+      await this.$store.dispatch('resetFilterState', this.resourceName)
 
-    /**
-     * Update the currentFilters with newFilters
-     */
-    updateFilters(newFilters) {
-      console.log('updating filters')
-      this.currentFilters = newFilters
-      // this.filterChanged()
-    },
-
-    /**
-     * Reset all of the current filters.
-     */
-    clearAllFilters() {
-      // this.currentFilters = []
-      // each(this.filters, filter => {
-      //   filter.currentValue = ''
-      // })
-    },
-
-    /**
-     * Sync the current filter values with the decoded filter query string values.
-     */
-    syncFilterValues() {
-      // each(this.filters, filter => {
-      //   filter.currentValue = get(
-      //     _(this.currentFilters).find(decoded => {
-      //       return filter.class == decoded.class
-      //     }),
-      //     'value',
-      //     filter.currentValue
-      //   )
-      // })
+      this.updateQueryString({
+        [this.pageParameter]: 1,
+        [this.filterParameter]: '',
+      })
     },
 
     /**
      * Handle a filter state change.
      */
     filterChanged() {
-      console.log('updating query string')
-      // this.updateQueryString({
-      //   [this.pageParameter]: 1,
-      //   [this.filterParameter]: btoa(JSON.stringify(this.currentFilters)),
-      // })
+      this.updateQueryString({
+        [this.pageParameter]: 1,
+        [this.filterParameter]: this.$store.getters.currentEncodedFilters,
+      })
+    },
+
+    /**
+     * Set up filters for the current view
+     */
+    async initializeFilters() {
+      await this.$store.dispatch('fetchFilters', this.resourceName)
+      this.initializeState()
+    },
+
+    /**
+     * Set up filters for the current lens view
+     */
+    async initializeLensFilters(lens) {
+      await this.$store.dispatch('fetchLensFilters', { resourceName: this.resourceName, lens })
+      this.initializeState()
+    },
+
+    /**
+     * Initialize the filter state
+     */
+    async initializeState() {
+      if (this.initialEncodedFilters) {
+        await this.$store.dispatch(
+          'initializeCurrentFilterValuesFromQueryString',
+          this.initialEncodedFilters
+        )
+      } else {
+        await this.$store.dispatch('resetFilterState', this.resourceName)
+      }
     },
   },
 
@@ -102,20 +62,6 @@ export default {
      */
     filterParameter() {
       return this.resourceName + '_filter'
-    },
-
-    /**
-     * Determine if there any filters for this resource
-     */
-    hasFilters() {
-      return Boolean(this.filters.length > 0)
-    },
-
-    /**
-     * Get the encoded filters from the query string.
-     */
-    encodedFilters() {
-      // return this.$route.query[this.filterParameter] || ''
     },
   },
 }
