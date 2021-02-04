9aa37de34b992132811446432e356d91f20921b4
Mior Muhammad Zaki <crynobone@gmail.com>
2021-02-04 18:24:53 +0800

    Add support for accented characters
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/util/singularOrPlural.js
+++ src/util/singularOrPlural.js
@@ -1,7 +1,7 @@
 import { Inflector } from '../'
 
 export default function singularOrPlural(value, suffix) {
-    if (value.match(/^(.*)[A-Za-z]$/) == null) return value
+    if (value.match(/^(.*)[A-Za-zÀ-ÖØ-öø-ÿ]$/) == null) return value
     else if (value > 1 || value == 0) return Inflector.pluralize(suffix)
     return Inflector.singularize(suffix)
 }
