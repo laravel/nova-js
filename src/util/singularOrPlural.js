e235eb5fac8dd42d31ac9e6423df476d128a6cda
Mior Muhammad Zaki <crynobone@gmail.com>
2021-04-22 07:47:05 +0800

    Fixes Value metric not handling suffix properly.
    * Check `suffix` to match string which shouldn't use inflector. (e.g: %
      and accented characters.
    * Check if `suffix` is actually string.
    
    Fixes laravel/nova-issues#3359
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/util/singularOrPlural.js
+++ src/util/singularOrPlural.js
@@ -1,7 +1,8 @@
 import { Inflector } from '../'
+import isString from 'lodash/isString'
 
 export default function singularOrPlural(value, suffix) {
-    if (value.match(/^(.*)[A-Za-zÀ-ÖØ-öø-ÿ]$/) == null) return value
+    if (isString(suffix) && suffix.match(/^(.*)[A-Za-zÀ-ÖØ-öø-ÿ]$/) == null) return suffix
     else if (value > 1 || value == 0) return Inflector.pluralize(suffix)
     return Inflector.singularize(suffix)
 }
